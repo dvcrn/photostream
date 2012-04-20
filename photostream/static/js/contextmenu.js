a6f77d34867fa15bcdc11c4cbe9a2d2a86672808
David Mohl <david.mohl@ymail.com>
2012-04-20 14:51:39 +0200

    First draggable select test

--- photostream/static/js/contextmenu.js
+++ photostream/static/js/contextmenu.js
@@ -12,7 +12,49 @@ var bindPhotoContextMenu = function() {
         {
 
         }
-});
+    });
+
+
+
+
+
+    $( library )
+    .drag("start",function( ev, dd ){
+        return $('<div class="selection" />')
+            .css('opacity', .65 )
+            .appendTo( document.body );
+    })
+    .drag(function( ev, dd ){
+        $( dd.proxy ).css({
+            top: Math.min( ev.pageY, dd.startY ),
+            left: Math.min( ev.pageX, dd.startX ),
+            height: Math.abs( ev.pageY - dd.startY ),
+            width: Math.abs( ev.pageX - dd.startX )
+        });
+    })
+    .drag("end",function( ev, dd ){
+        $( dd.proxy ).fadeOut(100, function() {
+
+        $( dd.proxy ).remove();
+        });
+    });
+
+    $("#library .photo")
+        .drop("start",function(){
+            $( this ).addClass("test2");
+        })
+        .drop(function( ev, dd ){
+            $( this ).toggleClass("test3");
+        })
+        .drop("end",function(){
+            $( this ).removeClass("test2");
+        });
+    $.drop({ multi: true });
+
+
+
+
+
 }
 
 var bindAlbumContextMenu = function() {

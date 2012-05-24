8bffea5146d9d448a20a2870ddee845ffd705167
David Mohl <david.mohl@ymail.com>
2012-05-24 11:52:50 +0200

    little bit of uploader style

--- photostream/static/js/website/script.js
+++ photostream/static/js/website/script.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
 		var url = $(this).attr("href");
 		window.open(url, '', 'height=750,width=1100', false);
 	});
+	$("#nav_launchuploader").click(function(e) {
+		e.preventDefault();
+		var url = $(this).attr("href");
+		window.open(url, '', 'height=400,width=600', false);
+	});
 });

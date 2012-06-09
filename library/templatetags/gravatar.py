14e33dd0eecc42cb4ce8915760d32489dd6bcbe8
David Mohl <david.mohl@ymail.com>
2012-06-10 01:20:13 +0900

    implemented gravatar avatar service. Removed 404 for missing avatar in the same step

---
+++ library/templatetags/gravatar.py
@@ -0,0 +1,14 @@
+from django import template
+import md5
+
+register = template.Library()
+
+@register.filter(name='gravatarUrl', is_safe=True)
+def thumburl(email):
+	m = md5.new()
+	m.update(email.lower())
+
+	digest = m.hexdigest()
+
+	url = "http://www.gravatar.com/avatar/%s.jpg?s=30" % digest
+	return url
